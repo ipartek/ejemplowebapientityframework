1d549ee8fa11695a83e20b8220be0529bee542dc
javierlete <jglete@gmail.com>
2018-03-07 15:38:51 +0100

    Modificación de registros, limpieza de campos y foco para siguiente inserción

--- BookService/Scripts/bookservice.js
+++ BookService/Scripts/bookservice.js
@@ -29,18 +29,45 @@ function formsubmit(e) {
         "AuthorId": $('#authors').val()
     };
 
-    console.log(JSON.stringify(book));
+    var bookId = $('#inputId').val();
+    var esActualizacion = !!bookId;
 
-    $.ajax({
-        url: url,
-        method: 'POST',
-        data: JSON.stringify(book),
-        dataType: 'json',
-        contentType: 'application/json'
-    }).done(function (libro) {
-        console.log(libro);
-        $.getJSON(url, libroscorrecto).fail(fallo);
-    }).fail(fallo);
+    if (esActualizacion) {
+        book.Id = bookId;
+
+        $.ajax({
+            url: url + "/" + bookId,
+            method: 'PUT',
+            data: JSON.stringify(book),
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (libro) {
+            console.log(libro);
+            limpiar();
+            $.getJSON(url, libroscorrecto).fail(fallo);
+        }).fail(fallo);
+    }
+    else
+        $.ajax({
+            url: url,
+            method: 'POST',
+            data: JSON.stringify(book),
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (libro) {
+            console.log(libro);
+            limpiar();
+            $.getJSON(url, libroscorrecto).fail(fallo);
+            $('#authors').focus();
+        }).fail(fallo);
+
+    console.log(JSON.stringify(book));
+}
+
+function limpiar() {
+    $('#inputId').remove();
+    $('#tituloform').text('Add Book');
+    $('#formbooks')[0].reset();
 }
 
 function autorescorrecto(autores) {
@@ -64,12 +91,34 @@ function libroscorrecto(libros) {
         $linea.find('.Title').text(libro.Title);
         $linea.find('.detalles').prop('href', url + "/" + libro.Id).click(librodetalle);
         $linea.find('.borrar').prop('href', url + "/" + libro.Id).click(libroborrar);
+        $linea.find('.actualizar').prop('href', url + "/" + libro.Id).click(libroactualizar);
 
         $books.append($linea);
 
         console.log(key, libro);
     });
 }
+
+function libroactualizar(e) {
+    e.preventDefault();
+
+    $.getJSON(this.href, function (libro) {
+        $('#authors').val(libro.AuthorId);
+        $('#inputTitle').val(libro.Title);
+        $('#inputYear').val(libro.Year);
+        $('#inputGenre').val(libro.Genre);
+        $('#inputPrice').val(libro.Price);
+
+        $('#tituloform').text('UPDATE BOOK');
+
+        if ($('#inputId').length)
+            $('#inputId').val(libro.Id);
+        else
+            $('#formbooks').
+                append('<input type="hidden" id="inputId" value="' + libro.Id + '" />');
+    });
+}
+
 
 function libroborrar(e) {
     e.preventDefault();
